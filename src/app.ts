dc4b361f49d7f022506474fb472efdd2faaac15a
FranciscoVitorNunes <franciscovitornunes.programador@gmail.com>
2025-09-08 02:13:34 -0300

    Implementção do Cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express, { json} from 'express'
 import router from './routes';
+import cors from "cors";
 
 export function createApp(){
     const app = express();
     app.use(json());
     app.use("/api", router)
+
+    const corsOptions = {
+        origin: '*',
+    }
+    app.use(cors(corsOptions))
     return app;
 }
